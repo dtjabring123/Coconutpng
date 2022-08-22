d68fd2f89e6d3b630567da2a48a9febe9907f494
Lord-Y14 <yoshailenmichael@gmail.com>
2022-08-22 21:29:37 +0200

    working on login page

---
+++ src/pages/login.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function login() {
+  return (
+    <div>
+        <>
+        <input type = "text"/>
+        <input type= "text"/>
+        <button>Login</button>
+
+        </>
+        
+    </div>
+  )
+}
