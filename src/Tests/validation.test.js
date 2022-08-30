25b2f26ab79a6b1896ae7fbab613d3f7d2d8492f
dtjabring123 <101052704+dtjabring123@users.noreply.github.com>
2022-08-30 16:59:55 +0200

    Update validation.test.js

--- src/Tests/validation.test.js
+++ src/Tests/validation.test.js
@@ -49,4 +49,58 @@ describe("validID tests", () => {
     test("valid ID", () => {
         expect(validation.validID("1234567891111")).toBe(true);
     })
+    test("ID with letters", () => {
+        expect(validation.validID("123456789111I")).toBe(false);
+    })
+});
+
+//tests for validName, tests for valid and invalid cases
+describe("validName tests", () => {
+    test("null name", () => {
+        expect(validation.validName("")).toBe(false);
+    })
+    test("numeric name", () => {
+        expect(validation.validName("Dylan1")).toBe(false);
+    })
+    test("valid name", () => {
+        expect(validation.validName("Dylan")).toBe(true);
+    })
+});
+
+//tests for validPhoneNum, tests for valid and invalid cases
+describe("validPhoneNum tests", () => {
+    test("null number", () => {
+        expect(validation.validPhoneNum("")).toBe(false);
+    })
+    test("number too long", () => {
+        expect(validation.validPhoneNum("12345678912345")).toBe(false);
+    })
+    test("number too short", () => {
+        expect(validation.validPhoneNum("123")).toBe(false);
+    })
+    test("valid number", () => {
+        expect(validation.validPhoneNum("0794049822")).toBe(true);
+    })
+    test("number with letters", () => {
+        expect(validation.validPhoneNum("q079404982")).toBe(false);
+    })
+});
+
+//tests for validDob, tests for valid and invalid cases
+describe("validDob tests", () => {
+    test("null date", () => {
+        expect(validation.validDob("")).toBe(false);
+    })
+    test("not a date", () => {
+        expect(validation.validDob("123asdas")).toBe(false);
+    })
+    test("date too recent", () => {
+        expect(validation.validDob("2015-03-25")).toBe(false);
+    })
+    test("date not recent", () => {
+        expect(validation.validDob("1015-03-25")).toBe(false);
+    })
+    test("valid date", () => {
+        expect(validation.validDob("2000-11-04")).toBe(true);
+    })
 });
