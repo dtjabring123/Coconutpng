c0eccd937e29ac5c8feddb90fbd9b727ae486db2
ebucs <okafornelson143@gmail.com>
2022-09-04 22:08:27 +0200

    upgraded welcome page

--- src/pages/mainscreen.jsx
+++ src/pages/mainscreen.jsx
@@ -1,27 +1,44 @@
 import React from 'react'
 import { Link } from "react-router-dom"
+import "../stylesheets/default.css";
 
 export default class WelcomePage extends React.Component {
   render(){
 		return (
-		<div>
-			<h1>Welcome to Coconut Overflow!</h1>
-      <div className="row">
-      <Link to="/login">      
-        <button className='buttonstyle'
-            style={{marginTop:10, marginBottom:30}}>
-            Login
-        </button>
-    </Link>
-    <Link to="/Register">      
-        <button className='buttonstyle'
-            style={{marginTop:10, marginBottom:30}}>
-            Register
-        </button>
-    </Link>
-      </div>
-    
-		</div>);
+    <div class="area" >
+          <ul class="circles">
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+          </ul>
+    <div class="context">
+      <h1>Welcome To Coconut Overflow</h1>
+      <div class="row">
+<div className="row">
+<Link to="/login">      
+<button className='buttonstyle'
+    style={{marginTop:10, marginBottom:30}}>
+    Login
+</button>
+</Link>
+<Link to="/Register">      
+<button className='buttonstyle'
+    style={{marginTop:10, marginBottom:30}}>
+    Register
+</button>
+</Link>
+</div>
+
+</div>
+    </div>
+    </div>);
 	}
   
 }
