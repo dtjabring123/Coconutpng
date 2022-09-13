34f4db6bad7a6d07f106d161834bd6ecc6743c93
dtjabring123 <101052704+dtjabring123@users.noreply.github.com>
2022-09-13 12:01:26 +0200

    Update login.test.js

--- src/Tests/login.test.js
+++ src/Tests/login.test.js
@@ -3,6 +3,6 @@ import {validLogin} from '../utils/login.js'
 //tests for validLogin, tests for valid and invalid cases
 describe("validLogin tests", () => {
     test("invalid email", () => {
-        expect(validLogin({ email: "1@gmail", password: "Qeys2tdfs12" })).toBe([false, "Email is not in the correct format \n"]);
+        expect(validLogin({ email: "1@gmail", password: "Qeys2tdfs12" })).toStrictEqual([false, "Email is not in the correct format \n"]);
     })
 });
