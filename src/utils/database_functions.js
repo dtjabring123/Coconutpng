eb712f23410725089538549a11bb66cf56b75a6b
NoName-0512 <duran.reddy13@gmail.com>
2022-09-18 15:50:18 +0200

    question_title fix

--- src/utils/database_functions.js
+++ src/utils/database_functions.js
@@ -529,7 +529,8 @@ async function getQuestionInfo(question_id){
     JSON = {
       "date": ret.data().question_date,
       "desc": ret.data().question_desc,
-      "likes": ret.data().question_title,
+      "likes": ret.data().question_likes,
+      "title": ret.data().question_title,
       "user_id":ret.data().question_user,
       "isQuestioner": ret.data().question_reference==auth.currentUser.email, //returns if they asked the question
       "liked":0 //default value means that the user did not like
