0a3d94c0c16bc0a3692420ea5d3031e823651ca0
Lord-Y14 <yoshailenmichael@gmail.com>
2022-09-19 20:02:48 +0200

    addResponse and display Response works

--- src/components/response_block.js
+++ src/components/response_block.js
@@ -1,17 +1,17 @@
 import React from "react";
 import "../stylesheets/comments.css";
 export default function ResponseBlock(props){
-    let date = props.props.title;
+    let date = props.props.title; //show
     let id = props.props.question_id;
-    let likes = props.props.likes;
-    let author = props.props.author;
-    let description = props.props.description;
+    let likes = props.props.likes; //show likes
+    let author = props.props.author; //show author
+    let description = props.props.description; //show description
     let marked = props.props.marked;
-    let question = props.props.question;
+    let question = props.props.question; //
 
     return(
         <div>
-
+            <label>{description}</label>
         </div>
     )
 }
--- src/pages/questionInfo.js
+++ src/pages/questionInfo.js
@@ -4,22 +4,31 @@ import { getQuestionInfo,getResponses,giveResponse_or_Comment } from "../utils/d
 import ResponseBlock from "../components/response_block";
 export default function QuestionInfo(){
     const location  = useLocation();
-    const [response_data,setResponse_data] = useState("");
-    initialiseValues(location.state.name); //start loading details about the question
+    const [response_data,setResponse_data] = useState(""); //stores user input for a response
+    const [changeQuestionDetails,setChangeQuestionDetails] = useState(false); //used to indicate if database should be queried for question details
+    const [response_list,setResponse_list] = useState([]); //stores list of responses to be displayed
+    const [changeResponseList,setChangeResponseList] = useState(false);//used to indicate if database should be queried for response list 
 
+
+    initialiseValues(location.state.name); //start loading details about the question
+    displayResponses(location.state.name);
     //get data from db about particular question
     function initialiseValues(question_id){
+        console.log(changeQuestionDetails)
+        if(changeQuestionDetails == false){ // don't fetch information from database about the question everytime page is rendered
         let succ = getQuestionInfo(question_id);
         Promise.resolve(succ).then((ret=>{
             if(ret[0] == 'success'){
                 //show received details
                 console.log("got question details");
                 console.log(ret[1]);
+                setChangeQuestionDetails(true);
                 displayDetails(ret[1]);
             }else{
-                console.log("Couldn't get info from database");
+                console.log("Couldn't get question info from database");
             }
         }))   
+    }
     }
     //set values to match data
     function displayDetails(details){
@@ -29,24 +38,28 @@ export default function QuestionInfo(){
         description_lbl.value = details.desc;
         //received date,desc,title,likes,liked,isQuestioner
         // from response list - response_id,date,description,likes,mark,user,isLiked
-        //getResponses(question_id,sorting_attribute,sorting_direction,startingValue,limit_num)
-        //giveResponse_or_Comment(0,id,desc) for adding a responsefvvfvvvbgvgbb
     }
 
     function  displayResponses(question_id){ //will display responses received from database
         //fetch responses from the database
+        if(changeResponseList == false){
         let succ = getResponses(question_id);
         Promise.resolve(succ).then((ret=>{
             if(ret[0] == 'success'){
                 //show received details
                 console.log("got responses");
                 console.log(ret[1]);
-                displayDetails(ret[1]);
+                 //save response array
+                setResponse_list(ret[1]);
+                //change flag for fetching response list 
+                setChangeResponseList(true);
+                console.log(response_list);
             }else{
-                console.log("Couldn't get info from database");
+                console.log("Couldn't get responses from database");
             }
         }))   
-        //set statevalue of list to recieved responses
+    }
+       
         //create responseblocks for each response
     }
     
@@ -54,16 +67,33 @@ export default function QuestionInfo(){
     const handleChange = e => { //updates response_data to match user input
         setResponse_data(e.target.value)
       }
-      //handle adding a response
+      //handle adding user adding a response
       function handleResponseAdd(){
         //check response is not empty
+        console.log(response_data);
         if((response_data != null) && (response_data.length > 0)){
             //call db method to add response
-            //output if successful or not
+            let succ = giveResponse_or_Comment(0,location.state.name,response_data);
+            Promise.resolve(succ).then((ret)=>{
+                    output("Comment added successfully");
+                    setChangeResponseList(false);
+                    var text_lbl = document.getElementById("input_field");
+                    text_lbl.value = "";
+            })
 
+        }else{
+           output("Your comment cannot be empty");
         }
       }
 
+      function output(message){ //output is given a message and displays a toast message of the input
+		var x = document.getElementById("snackbar");
+		x.className = "show";
+		x.innerHTML = message;
+		setTimeout(function () {
+			x.className = x.className.replace("show", "");
+		}, 3000);
+	}
     return(
         <form>
         <div id = "snackbar"></div>
@@ -77,17 +107,24 @@ export default function QuestionInfo(){
 
             <div className="q-group">
                 <label htmlFor="description">Post Answer</label>
-                <textarea className="texta" placeholder="Type your answer here" onChange={evt=>handleChange(evt)} />
-                <input id = "answer" type = "button" value = "Submit" onClick={handleResponseAdd()}/>
+                <textarea id="input_field" className="texta" placeholder="Type your answer here" onChange={evt=>handleChange(evt)} />
+                <input id = "answer" type = "button" value = "Submit" onClick={()=>handleResponseAdd()}/>
                 <Link to="/homepage">      
                     <button type="submit" value="Submit" id = "home_button"/>
                </Link>
             </div>
-            <ResponseBlock props = {{data : ""}} key = "key1"/>
+            {
+                response_list.map((response)=>{
+                    if((response.description != null) && (response.description.length > 0)){
+                        return(<ResponseBlock props = {response} key = {response.id}/>)
+                    }
+                })
+            } 
             <Link to="/homepage">      
                 <input type="submit" value="BACK"/>
                </Link>
             </div>
+
     </form>
     )
 }
