1ec35400dd27ad8b5f74e32754263be1b2b07083
ebucs <okafornelson143@gmail.com>
2022-09-19 20:30:48 +0200

    fixed some bugs

--- src/components/response_block.js
+++ src/components/response_block.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "../stylesheets/comments.css";
 export default function ResponseBlock(props){
-    let date = props.props.title; //show
+    let date = props.props.date; //show
     let id = props.props.question_id;
     let likes = props.props.likes; //show likes
     let author = props.props.author; //show author
@@ -13,35 +13,13 @@ export default function ResponseBlock(props){
         <div className="container2">
             <div class="response_container">
                 <div className='response_card'>
-                    <h3 className="head2">Response by: Me</h3>
+                    <h3 className="head2">Response by: {author}</h3>
                     <p className="par">
-                    This form has the default HTML form behavior of browsing to a new page when the user submits the form. If you want this behavior in React, it just works. But in most cases, it’s convenient to have a JavaScript function that handles the submission of the form and has access to the data that the user entered into the form. The standard way to achieve this is with a technique called “controlled components”.
+                        {description}
                     </p>
                     <div className='response_card-footer'>
-                        <div> Answered on the: 12 Sep 2022 </div>
-                        <div> 12 Likes</div>
-                    </div>
-                </div>
-
-                <div className='response_card'>
-                    <h3 className="head2">Response by: Me</h3>
-                    <p className="par">
-                    This form has the default HTML form behavior of browsing to a new page when the user submits the form. If you want this behavior in React, it just works. But in most cases, it’s convenient to have a JavaScript function that handles the submission of the form and has access to the data that the user entered into the form. The standard way to achieve this is with a technique called “controlled components”.
-                    </p>
-                    <div className='response_card-footer'>
-                        <div> Answered on the: 12 Sep 2022 </div>
-                        <div> 12 Likes</div>
-                    </div>
-                </div>
-
-                <div className='response_card'>
-                    <h3 className="head2">Response by: Me</h3>
-                    <p className="par">
-                    This form has the default HTML form behavior of browsing to a new page when the user submits the form. If you want this behavior in React, it just works. But in most cases, it’s convenient to have a JavaScript function that handles the submission of the form and has access to the data that the user entered into the form. The standard way to achieve this is with a technique called “controlled components”.
-                    </p>
-                    <div className='response_card-footer'>
-                        <div> Answered on the: 12 Sep 2022 </div>
-                        <div> 12 Likes</div>
+                        <div> Answered on: {date} </div>
+                        <div> {likes} Likes</div>
                     </div>
                 </div>
 
